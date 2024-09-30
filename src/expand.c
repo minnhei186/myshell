23c4e57d4dced20882b647fe000f610e3aeefe7b
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2024-09-30 14:23:13 +0900

    mk30_expand_ready_for_variable_expander

--- src/expand.c
+++ src/expand.c
@@ -6,19 +6,37 @@
 /*   By: hosokawa <hosokawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:36:42 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/09/30 14:00:05 by hosokawa         ###   ########.fr       */
+/*   Updated: 2024/09/30 14:22:46 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "myshell.h"
 
-//ひとつ一つのtokenを見ていく。
-//サイズが大きくてもちゃんと終端文字があればいいか。
-//つまりそう、別に大きくてもいいかもね。
+void	remove_single_quote(char *new_word, char *old_word, int *i, int *j)
+{
+	*i = (*i) + 1;
+	while (old_word[*i] != '\'')
+	{
+		new_word[*j] = old_word[*i];
+		*j = (*j) + 1;
+		*i = (*i) + 1;
+	}
+	*i = (*i) + 1;
+}
 
-// tokenはそのままに、tokenのwordだけを変更か、こうすれば新しくメモリ空間をその構造体分確保しなくてもいい、その構造体のアドレスを格納している場所が消失しないですむ。
-//関数の移動において。
+void	remove_double_quote(char *new_word, char *old_word, int *i, int *j)
+{
+	*i = (*i) + 1;
+	while (old_word[*i] != '\"')
+	{
+		new_word[*j] = old_word[*i];
+		*j = (*j) + 1;
+		*i = (*i) + 1;
+	}
+	*i = (*i) + 1;
+}
 
+//あくまでも、ここでのポインターだけを動かす、ということ
 void	remove_quote(t_token_info *token)
 {
 	char	*new_word;
@@ -28,38 +46,20 @@ void	remove_quote(t_token_info *token)
 
 	old_word = token->word;
 	i = strlen(old_word);
-	new_word = calloc(i + 1, sizeof(char)); 
+	new_word = calloc(i + 1, sizeof(char));
 	i = 0;
 	j = 0;
 	while (old_word[i])
 	{
 		if (old_word[i] == '\'')
+			remove_single_quote(new_word, old_word, &i, &j);
+		else if (old_word[i] == '\"')
+			remove_double_quote(new_word, old_word, &i, &j);
+		else
 		{
+			new_word[j] = old_word[i];
+			j++;
 			i++;
-			while (old_word[i] != '\'')
-			{
-				new_word[j] = old_word[i];
-				j++;
-				i++;
-			}
-			i++;
-		}
-		else if(old_word[i] == '\"')
-		{
-			i++;
-			while (old_word[i] != '\"')
-			{
-				new_word[j] = old_word[i];
-				j++;
-				i++;
-			}
-			i++;
-		}
-		else 
-		{
-		new_word[j] = old_word[i];
-		j++;
-		i++;
 		}
 	}
 	free(token->word);
@@ -70,8 +70,8 @@ void	remove_quote(t_token_info *token)
 //ただし、そもそも処理されていないtokenもある
 void	token_quote_removal(t_token_info *token)
 {
-	if(token==NULL)
-		return;
+	if (token == NULL)
+		return ;
 	while (token->next != NULL)
 	{
 		if ((token->kind == WORD) && (token->word != NULL))
@@ -80,29 +80,48 @@ void	token_quote_removal(t_token_info *token)
 	}
 }
 
-void quote_remover(t_node_info *node)
+void	quote_remover(t_node_info *node)
 {
-	if(node==NULL)
-		return;
-
+	if (node == NULL)
+		return ;
 	token_quote_removal(node->node_token);
 	token_quote_removal(node->filename);
 	token_quote_removal(node->delimiter);
 	quote_remover(node->redirects);
 	quote_remover(node->cmd);
 	quote_remover(node->re_node);
-
 }
-	
 
-///name_value
+/// name_value
 
+ void	token_variable_expand(t_token_info *token)
+{
+	if(token==NULL)
+		return ;
+	while (token->next != NULL)
+	{
+		if ((token->kind == WORD) && (token->word != NULL))
+			expand_variable(token);
+		token = token->next;
+	}
+}
+
+
+
+ void variable_expander(t_node_info *node)
+{
+	if(node==NULL)
+		return ;
+
+	token_variable_expand(node->node_token);
+	token_variable_expand(node->filename);
+	variable_expander(node->redirects);
+	variable_expander(node->cmd);
+	variable_expander(node->re_node);
+}
 
 void	expand(t_node_info *node)
 {
+	// variable_expander(node);
 	quote_remover(node);
 }
-
-
-
-
