5c2b5072fc59fad6cb4ab075f9f0aa6a3132637b
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2024-10-02 10:27:11 +0900

    mk34_expand_variable_single_quote

--- src/expand.c
+++ src/expand.c
@@ -6,66 +6,119 @@
 /*   By: hosokawa <hosokawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:36:42 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/09/30 15:51:22 by hosokawa         ###   ########.fr       */
+/*   Updated: 2024/10/01 19:42:57 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "myshell.h"
 
-void	remove_single_quote(char *new_word, char *old_word, int *i, int *j)
+char	*ft_char_recalloc(char *old, size_t count)
 {
-	*i = (*i) + 1;
-	while (old_word[*i] != '\'')
+	char	*new;
+	size_t	i;
+
+	if (count == SIZE_MAX)
+		return (NULL);
+	if (old == NULL)
 	{
-		new_word[*j] = old_word[*i];
-		*j = (*j) + 1;
-		*i = (*i) + 1;
+		new = ft_calloc(count + 1, sizeof(char));
+		if (new == NULL)
+			return (NULL);
+		return (new);
 	}
-	*i = (*i) + 1;
+	i = ft_strlen(old);
+	if (i > SIZE_MAX - count - 1) //右辺が-1だとラウンドアップされ検出できない
+		return (NULL);
+	new = ft_calloc(i + count + 1, sizeof(char));
+	if (new == NULL)
+		return (NULL);
+	strlcpy(new, old, i + 1); // null終端文字を含めること
+	free(old);
+	return (new);
 }
 
-void	remove_double_quote(char *new_word, char *old_word, int *i, int *j)
+// oldはエラーの時消さずに持ってこう。
+//うわ、いいねこのありようによって。おもしろ
+// oldがNULLの時は新しく作る。
+char	*append_char(char *old, char append_c)
 {
-	*i = (*i) + 1;
-	while (old_word[*i] != '\"')
+	char	*new;
+	int		i;
+
+	//成功した場合はoldは消える。oldに元のところでしてしまおう
+	new = ft_char_recalloc(old, 1);
+	if (new == NULL)
+		return (NULL);
+	if (old == NULL)
+		new[0] = append_c;
+	else
 	{
-		new_word[*j] = old_word[*i];
-		*j = (*j) + 1;
-		*i = (*i) + 1;
+		//すでにoldはfreeずみ
+		i = ft_strlen(new);
+		new[i] = append_c;
 	}
-	*i = (*i) + 1;
+	return (new);
 }
 
+// pptは格納庫のアドレス *pptでその格納庫の中に入れることができるし中のものにアクセスしている；
+#define SINGLE_QUOTE '\''
+#define DOUBLE_QUOTE '\"'
+
+char	*remove_single_quote(char **word_ppt, char *new_word)
+{
+	(*word_ppt) = (*word_ppt) + 1;
+	while (**word_ppt != SINGLE_QUOTE)
+	{
+		new_word = append_char(new_word, **word_ppt);
+		if (new_word == NULL)
+			return (NULL);
+		(*word_ppt) = (*word_ppt) + 1;
+	}
+	(*word_ppt) = (*word_ppt) + 1;
+	return (new_word);
+}
+
+char	*remove_double_quote(char **word_ppt, char *new_word)
+{
+	(*word_ppt) = (*word_ppt) + 1;
+	while (**word_ppt != DOUBLE_QUOTE)
+	{
+		new_word = append_char(new_word, **word_ppt);
+		if (new_word == NULL)
+			return (NULL);
+		(*word_ppt) = (*word_ppt) + 1;
+	}
+	(*word_ppt) = (*word_ppt) + 1;
+	return (new_word);
+}
+
+// freeしてしまうのは問題か？
+// freeしなくてもいいかもな（realloc)
+//いや、そうか新しく作るものをreallocしていけば、freeは必要
+// oldが存在していたら新しく作成しよう。
 void	remove_quote(t_token_info *token)
 {
+	char	*word;
 	char	*new_word;
-	char	*old_word;
-	int		i;
-	int		j;
 
-	old_word = token->word;
-	i = strlen(old_word);
-	new_word = calloc(i + 1, sizeof(char));
-	i = 0;
-	j = 0;
-	while (old_word[i])
+	word = token->word;
+	new_word = NULL;
+	while (*word)
 	{
-		if (old_word[i] == '\'')
-			remove_single_quote(new_word, old_word, &i, &j);
-		else if (old_word[i] == '\"')
-			remove_double_quote(new_word, old_word, &i, &j);
+		if (*word == SINGLE_QUOTE)
+			new_word = remove_single_quote(&word, new_word);
+		else if (*word == DOUBLE_QUOTE)
+			new_word = remove_double_quote(&word, new_word);
 		else
 		{
-			new_word[j] = old_word[i];
-			j++;
-			i++;
+			new_word = append_char(new_word, *word);
+			word++;
 		}
 	}
 	free(token->word);
 	token->word = new_word;
 }
 
-//ただし、そもそも処理されていないtokenもある
 void	token_quote_removal(t_token_info *token)
 {
 	if (token == NULL)
@@ -90,62 +143,129 @@ void	quote_remover(t_node_info *node)
 	quote_remover(node->re_node);
 }
 
-/// name_value
+// countを符号なしの64bit範囲で考える
+// reallocではなくて、calloc側の処理にした方が良いかも
+//それはおk、しかし計算というのはその領域を超える可能性を常に持っている。
+//計算は今を超えていくのである。
+//
+bool	is_alpha_or_under(char c)
+{
+	return (ft_isalpha(c) || c == '_');
+}
+bool	is_alpha_or_under_or_digit(char c)
+{
+	return (is_alpha_or_under(c) || ft_isdigit(c));
+}
 
-//void	expand_variable(t_token_info *token)
-//{
-//	char	*new_word;
-//	char	*old_word;
-//	int		i;
-//	int		j;
-//
-//	old_word = token->word;
-//	i = strlen(old_word);
-//	new_word = calloc(i + 1, sizeof(char));
-//	i = 0;
-//	j = 0;
-//	while (old_word[i])
-//	{
-//		if (old_word[i] == '\'')
-//			remove_single_quote(new_word, old_word, &i, &j);
-//		else if (old_word[i] == '\"')
-//			remove_double_quote(new_word, old_word, &i, &j);
-//		else
-//		{
-//			new_word[j] = old_word[i];
-//			j++;
-//			i++;
-//		}
-//	}
-//	free(token->word);
-//	token->word = new_word;
-//}
-//
-//void	token_variable_expand(t_token_info *token)
-//{
-//	if (token == NULL)
-//		return ;
-//	while (token->next != NULL)
-//	{
-//		if ((token->kind == WORD) && (token->word != NULL))
-//			expand_variable(token);
-//		token = token->next;
-//	}
-//}
-//
-//void	variable_expander(t_node_info *node)
-//{
-//	if (node == NULL)
-//		return ;
-//	token_variable_expand(node->node_token);
-//	token_variable_expand(node->filename);
-//	variable_expander(node->redirects);
-//	variable_expander(node->cmd);
-//	variable_expander(node->re_node);
-//}
-//
+bool	is_variable(char *word)
+{
+	if (word[0] == '$' && is_alpha_or_under(word[1]))
+		return (true);
+	return (false);
+}
+
+//ここのエラー処理注意
+char	*expand_variable_word(char **word, char *new_word)
+{
+	char	*variable_name;
+	char	*variable_word;
+
+	variable_name = NULL;
+	variable_word = NULL;
+	// skip_$
+	*word = *word + 1;
+	if (is_alpha_or_under(**word) != 1)
+		printf("need_alpha_or_under_front_of_str\n");
+	//途中で'などが出てきたら、wordは動かさない。
+	while (is_alpha_or_under_or_digit(**word))
+	{
+		variable_name = append_char(variable_name, **word);
+		*word = *word + 1;
+	}
+	variable_word = getenv(variable_name);
+	if (variable_word != NULL)
+	{
+		while (*variable_word != '\0')
+		{
+			new_word = append_char(new_word, *variable_word);
+			variable_word++;
+		}
+	}
+	return (new_word);
+}
+
+char *expand_variable_single_quote(char **word,char *new_word)
+{
+	//append_and_skip_single_quote
+	new_word=append_char(new_word,**word);
+	*word=*word+1;
+	while(**word!=SINGLE_QUOTE)
+	{
+		if(**word=='\0')
+		{
+			printf("not_close_single_quote\n");
+			return NULL;
+		}
+		new_word=append_char(new_word,**word);
+		*word=*word+1;
+	}
+	
+	new_word=append_char(new_word,**word);
+	*word=*word+1;
+	return new_word;
+}
+
+
+void	expand_variable(t_token_info *token)
+{
+	char	*word;
+	char	*new_word;
+
+	word = token->word;
+	new_word = NULL;
+	while (*word)
+	{
+		 if (*word == SINGLE_QUOTE)
+			new_word = expand_variable_single_quote(&word, new_word);
+		// else if (*word == DOUBLE_QUOTE)
+		//	new_word = expand_variable_double_quote(&word, new_word);
+		else if (is_variable(word))
+			new_word = expand_variable_word(&word, new_word);
+		else
+		{
+			new_word = append_char(new_word, *word);
+			word++;
+		}
+	}
+	free(token->word);
+	token->word = new_word;
+}
+
+void	token_variable_expand(t_token_info *token)
+{
+	if (token == NULL)
+		return ;
+	while (token->next != NULL)
+	{
+		if ((token->kind == WORD) && (token->word != NULL))
+			expand_variable(token);
+		token = token->next;
+	}
+}
+
+void	variable_expander(t_node_info *node)
+{
+	if (node == NULL)
+		return ;
+	token_variable_expand(node->node_token);
+	token_variable_expand(node->filename);
+	variable_expander(node->redirects);
+	variable_expander(node->cmd);
+	variable_expander(node->re_node);
+}
+
 void	expand(t_node_info *node)
 {
-	// variable_expander(node);
+	variable_expander(node);
 	quote_remover(node);
 }
