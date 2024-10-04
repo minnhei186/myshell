1babcdf3ad585a5e5a2e093135ce06236b2a5c58
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2024-10-04 18:05:26 +0900

    mk36_exit_builtin

--- src/info_data.c
+++ src/info_data.c
@@ -6,20 +6,11 @@
 /*   By: hosokawa <hosokawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 11:52:26 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/10/04 10:44:48 by hosokawa         ###   ########.fr       */
+/*   Updated: 2024/10/04 16:53:44 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "myshell.h"
-
-//ユーザーエラーはフラグがたつかどうかで考えていく。
-//ということはこのshell_map.cも考えなおそう。intはvoidでいい。
-// infoのフラグだけを立てておこう。
-
-// infoに依存しすぎる。依存というか、ある空間とその空間における存在について
-//空間と存在について、存在は空間というものが。もちろん、これはひとつとして。
-
-//ここではそれを1しようか。そこから？
 
 void	info_null_init(t_prompt_info *info)
 {
@@ -31,11 +22,6 @@ void	info_null_init(t_prompt_info *info)
 	info->map = NULL;
 }
 
-// empty_valueとは何か、yourser_err？処理の中止？
-// valueなくてもnmaeを作成するかどうかだな。なるほどね。
-//次を見るのか？
-//処理は中止すべきか？
-//とりあえず、中止することにしようかな。
 void	info_envp2item(t_prompt_info *info, char **envp)
 {
 	int	i;
--- src/readline.c
+++ src/readline.c
@@ -6,7 +6,7 @@
 /*   By: hosokawa <hosokawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 12:34:01 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/10/04 10:36:11 by hosokawa         ###   ########.fr       */
+/*   Updated: 2024/10/04 17:50:28 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,12 +67,103 @@ int	wait_process(int last_pid)
 	return (status);
 }
 
+
+
+
+
+
+//bultin!!!
+//////////////////////////
+//////////////////////////
+//////////////////////////
+//////////////////////////
+
+bool	is_numeric(char *s)
+{
+	if (!isdigit(*s))
+		return (false);
+	while (*s)
+	{
+		if (!isdigit(*s))
+			return (false);
+		s++;
+	}
+	return (true);
+}
+
+int	builtin_exit(t_prompt_info *info,char **argv)
+{
+	long	res;
+	char	*arg;
+	char	*endptr;
+
+	if (argv[1] == NULL)
+		exit(info->last_status);
+	if (argv[2])
+	{
+		return (1);
+	}
+	arg = argv[1];
+	if (is_numeric(arg))
+	{
+		errno = 0;
+		res = strtol(arg, &endptr, 10);
+		if (errno == 0 && *endptr == '\0')
+			exit((int)res);
+	}
+	exit(255);
+}
+
+bool	is_builtin(t_node_info *node)
+{
+	const char		*cmd_name;
+	char			*builtin_commands[] = {"exit", "export", "unset", "env",
+					"cd", "echo", "pwd"};
+	unsigned int	i;
+
+	if (node == NULL || node->cmd == NULL | node->cmd->node_token== NULL
+		|| node->cmd->node_token->word == NULL)
+		return (false);
+	cmd_name = node->cmd->node_token->word;
+	i = 0;
+	while (i < sizeof(builtin_commands) / sizeof(*builtin_commands))
+	{
+		if (strcmp(cmd_name, builtin_commands[i]) == 0)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+void	exec_bultin(t_prompt_info *info, t_node_info *node)
+{
+	char	**cmd_argv;
+	int		status;
+
+	status = 0;
+	do_redirect(node);
+	cmd_argv = token2argv(node->cmd->node_token);
+	if (ft_strncmp(cmd_argv[0], "exit", 4) == 0)
+	{
+		status = builtin_exit(info, cmd_argv);
+		info->last_status = status;
+	}
+	free(cmd_argv);
+	reset_redirect(node);
+	return ;
+}
+
 void	exec(t_prompt_info *info, t_node_info *node)
 {
 	int	last_pid;
 
-	last_pid = command_comunication(node);
-	info->last_status = wait_process(last_pid);
+	if (node->re_node == NULL && is_builtin(node))
+		exec_bultin(info, node);
+	else
+	{
+		last_pid = command_comunication(node);
+		info->last_status = wait_process(last_pid);
+	}
 }
 
 void	shell_operation(t_prompt_info *info, t_operation_info *operation)
